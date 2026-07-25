f6a5e0e2b8206f4c95576b94fa119d7b6ff6721d
Malcomian <malcolmwm@gmail.com>
2026-07-25 15:55:29 -0700

    fixing line length formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,9 +1,14 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters = {
-      prettier = {
-        prepend_args = { "--print-width", "10000" },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        html = { "prettier" }, -- this makes Prettier the primary formatter
+      },
+      formatters = {
+        prettier = {
+          prepend_args = { "--print-width", "10000" },
+        },
       },
     },
   },
